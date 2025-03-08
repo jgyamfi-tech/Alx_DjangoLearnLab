ae11607e23f8114517686f397721e32b3e2314f9
gyamfijoseph <gyamfijoseph65@gmail.com>
2025-03-08 23:28:04 +0000

    Added API endpoint for books

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    published_date = models.DateField() 
+    published_date = models.DateField(default="2000-01-01")
 
     def __str__(self):
         return self.title
