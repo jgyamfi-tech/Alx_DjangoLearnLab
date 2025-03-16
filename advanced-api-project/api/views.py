d34602ff8c5c079a7caaa5b1a5793f297508fecb
gyamfijoseph <gyamfijoseph65@gmail.com>
2025-03-16 07:01:42 +0000

    Added generic views for Book model with permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from api.models import Book
+from api.serializers import BookSerializer
 
 # Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    Handles listing all books and creating a new book.
+    Read access is open to everyone, but only authenticated users can create.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAuthenticated()]
+        return []
+
+#  Retrieve, Update, Delete Views
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles retrieving, updating, and deleting a single book.
+    Read access is open to everyone, but only authenticated users can update/delete.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return []
