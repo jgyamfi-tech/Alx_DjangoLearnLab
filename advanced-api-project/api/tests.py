60fb20a2a5089157e988cc3afa948f27ca71d54c
gyamfijoseph <gyamfijoseph65@gmail.com>
2025-03-16 19:45:05 +0000

    Updated views and added unit tests

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from api.models import Book, Author
 
 # Create your tests here.
+class BookTestCase(TestCase):
+    def setUp(self):
+        """Set up initial test data"""
+        self.author = Author.objects.create(name="Test Author")
+        self.book = Book.objects.create(
+            title="Test Book", publication_year=2024, author=self.author
+        )
+
+    def test_book_creation(self):
+        """Test that a book is created successfully"""
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(self.book.title, "Test Book")
